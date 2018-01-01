db4e565b771326667a1b49c80e755b4044b47684
iwilliams83 <indirajamurath@gmail.com>
2018-01-01 16:37:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,11 +5,15 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM characters ORDER BY length(motto) DESC LIMIT 1"
 end
-#INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1),
+#INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES 
+#(1, "Lady", "Woof Woof", "direwolf", 1, 1),
 #(2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1),
-#(3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
-#(5, "Character One", "motto one", "cylon", 2, 2), (6, "Character Two", "motto two", "human", 2, 2),
-#(7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
+#(3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), 
+#(4, "Eddard Stark", "Winter is coming", "human", 1, 1);
+#(5, "Character One", "motto one", "cylon", 2, 2), 
+#(6, "Character Two", "motto two", "human", 2, 2),
+#(7, "Character Three", "motto three", "cylon", 2, 2), 
+#(8, "Character Four", "motto four", "cylon", 2, 2);
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) FROM characters WHERE species = 'human'"
@@ -21,7 +25,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series INNER JOIN characters ON
+  characters.series_id = series.id WHERE characters.species = 'human'
+  ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
