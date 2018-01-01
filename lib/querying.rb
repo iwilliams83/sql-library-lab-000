20582ebd0e6cf3aa640539fc3ef18a12f2ec1510
iwilliams83 <indirajamurath@gmail.com>
2018-01-01 16:11:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,8 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, sub-genres.name FROM authors
+  INNER JOIN sub-genres ON sub-genres.id = authors.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
