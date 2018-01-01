0e2e0755b3f943a1a1243fb9c049ab9b140314d6
iwilliams83 <indirajamurath@gmail.com>
2018-01-01 17:24:35 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,15 +20,6 @@ def select_series_title_with_most_human_characters
   characters.series_id = series.id WHERE characters.species = 'human'
   GROUP BY series.title ORDER BY (COUNT(characters.species)) DESC LIMIT 1"
 end
-#INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
-  #(1, "Lady", "Woof Woof", "direwolf", 1, 1),
-  #(2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1),
-  #(3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1),
-  #(4, "Eddard Stark", "Winter is coming", "human", 1, 1);
-  #(5, "Character One", "motto one", "cylon", 2, 2),
-  #(6, "Character Two", "motto two", "human", 2, 2),
-  #(7, "Character Three", "motto three", "cylon", 2, 2),
-  #(8, "Character Four", "motto four", "cylon", 2, 2);
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id) FROM characters
