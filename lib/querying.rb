b3c032d15b7f09b43694c31f5eb7e95ba9eb279d
iwilliams83 <indirajamurath@gmail.com>
2018-01-01 16:14:32 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
-  INNER JOIN subgenres ON subgenres.id = authors.subgenre_id"
+  INNER JOIN subgenres ON subgenres.id = authors.id"
 end
 
 def select_series_title_with_most_human_characters
