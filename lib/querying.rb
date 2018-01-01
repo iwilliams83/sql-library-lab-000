180273769ad972852de32480d896d85cde22aca2
iwilliams83 <indirajamurath@gmail.com>
2018-01-01 16:37:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,14 +5,14 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM characters ORDER BY length(motto) DESC LIMIT 1"
 end
-#INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES 
+#INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
 #(1, "Lady", "Woof Woof", "direwolf", 1, 1),
 #(2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1),
-#(3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), 
+#(3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1),
 #(4, "Eddard Stark", "Winter is coming", "human", 1, 1);
-#(5, "Character One", "motto one", "cylon", 2, 2), 
+#(5, "Character One", "motto one", "cylon", 2, 2),
 #(6, "Character Two", "motto two", "human", 2, 2),
-#(7, "Character Three", "motto three", "cylon", 2, 2), 
+#(7, "Character Three", "motto three", "cylon", 2, 2),
 #(8, "Character Four", "motto four", "cylon", 2, 2);
 
 def select_value_and_count_of_most_prolific_species
