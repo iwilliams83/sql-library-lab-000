2c7420de78a379cc1d731d47d45c4c44536c8c96
iwilliams83 <indirajamurath@gmail.com>
2018-01-01 17:08:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,15 +5,6 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM characters ORDER BY length(motto) DESC LIMIT 1"
 end
-#INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
-#(1, "Lady", "Woof Woof", "direwolf", 1, 1),
-#(2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1),
-#(3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1),
-#(4, "Eddard Stark", "Winter is coming", "human", 1, 1);
-#(5, "Character One", "motto one", "cylon", 2, 2),
-#(6, "Character Two", "motto two", "human", 2, 2),
-#(7, "Character Three", "motto three", "cylon", 2, 2),
-#(8, "Character Four", "motto four", "cylon", 2, 2);
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) FROM characters WHERE species = 'human'"
@@ -29,7 +20,19 @@ def select_series_title_with_most_human_characters
   characters.series_id = series.id WHERE characters.species = 'human'
   GROUP BY series.title ORDER BY (COUNT(characters.species)) DESC LIMIT 1"
 end
-
+#INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
+  #(1, "Lady", "Woof Woof", "direwolf", 1, 1),
+  #(2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1),
+  #(3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1),
+  #(4, "Eddard Stark", "Winter is coming", "human", 1, 1);
+  #(5, "Character One", "motto one", "cylon", 2, 2),
+  #(6, "Character Two", "motto two", "human", 2, 2),
+  #(7, "Character Three", "motto three", "cylon", 2, 2),
+  #(8, "Character Four", "motto four", "cylon", 2, 2);
+#INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1),
+  #(2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), 
+  #(5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books) FROM characters INNER JOIN books
+  ON characters.series_id = books.series_id"
 end
